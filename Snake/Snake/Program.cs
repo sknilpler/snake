eb73c27e2511d647116aaabe9393a2d61293e4a5
sknilpler <nesterov.craft.29@gmail.com>
2020-01-29 21:34:49 +0500

    my first commit after 7 lessons with done homework

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            HorizontalLine hLine = new HorizontalLine(5,10,8,'+');
+            hLine.Drow();
+
+            VerticalLine vLine = new VerticalLine(9,15,5,'+');
+            vLine.Drow();
+
+            Console.ReadLine();
+        }
+
+    }
+}
